c12c6685662cb84ab6d21a5dd9f74da918234dc2
Berke Ates <berke@ates.ch>
2021-03-07 16:33:44 +0100

    new line adjust

--- cms/modules/logger/logger.php
+++ cms/modules/logger/logger.php
@@ -13,7 +13,7 @@ class Logger{
     if(!$config->savelog){ return; }
 
     $log = fopen($root . "logs.txt", "a");
-    fwrite($log, "\n". $text);
+    fwrite($log, "\r\n". $text);
     fclose($log);
   }
 }
